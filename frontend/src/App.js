5930622cc43029872bc78186faf7d597c609cb9a
Egor <egorsavvinovv2003@yandex.ru>
2024-04-02 15:40:35 +1000

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ApiComponent from './api/api';
+
+function App() {
+  return (
+    <div className="App">
+      <ApiComponent/>
+    </div>
+  );
+}
+
+export default App;
