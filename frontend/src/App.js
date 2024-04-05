68b77ea679571030dfd717a09b2d8500c368129f
Egor <egorsavvinovv2003@yandex.ru>
2024-04-05 13:32:22 +1000

    successful authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
-import ApiComponent from './api/api';
+import Test from './api/api';
 
 function App() {
+  // function getMainActorProfileFromMovie(id) {
+  //   return fetch(`https://swapi.dev/api/films/${id}/`)
+  //     .then((movieResponse) => {
+  //       return movieResponse.json()
+  //     })
+  //     .then((movie) => {
+  //       const characterUrl = movie.characters[0].split("//")[1]
+  //       return fetch(`https://${characterUrl}`)
+  //     })
+  //     .then((characterResponse) => {
+  //       return characterResponse.json()
+  //     })
+  //     .catch((err) => {
+  //       console.error("Произошла ошибка!", err)
+  //     })
+  // }
+  
+  // getMainActorProfileFromMovie(1).then((profile) => {
+  //   console.log(profile)
+  // })
   return (
     <div className="App">
-      <ApiComponent/>
+      <Test/>
     </div>
   );
 }
--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,80 +1,108 @@
-import React from "react";
-
-// export default class ApiComponent extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             error: null,
-//             isLoaded: false,
-//             items: []
-//         }
-//     }
-
-//     componentDidMount(){
-//         fetch("http://localhost:8002/api/v1/get_card/1")
-//         .then(res => res.json())
-//         .then(
-//             (result) => {
-//                 this.setState({
-//                     isLoaded: true,
-//                     items: result
-//                 })
-//             },
-//             (error) => {
-//                 this.setState({
-//                     isLoaded: true,
-//                     error
-//                 })
-//             }
-//         )
-//     }
-//     render(){
-//         const {error, isLoaded, items} = this.state
-//         if (error){
-//             return <p>Error {error.message}</p>
-//         }
-//         else if (!isLoaded) {
-//             return <p>Loading... </p>
-//         }
-//         else{
-//             return (
-//                 <div>
-//                     {items}
-//                 </div>
-//             )
-//         }
-//     }
-// }
-
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-let instance = axios.create(
+// api.interceptors.request.use((config) => {
+//     config.headers.Authorization = `Bearer ${token}`
+//     return config
+// })
+
+// axios.get(webApiUrl, { headers: {"Authorization" : `Bearer ${tokenStr}`} });
+// axios.defaults.headers.common["Content-Type"] = "application/json";
+// axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijk5OSIsImV4cCI6MTcxMjE0NTc0MH0.VXnnU4pr8Gc7GFViT77qvJN3eMJcsHyDww0JBeWBcuw';
+// api.interceptors.request.use(
+//     config => {
+//       config.headers['Authorization'] = `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijk5OSIsImV4cCI6MTcxMjE0NTc0MH0.VXnnU4pr8Gc7GFViT77qvJN3eMJcsHyDww0JBeWBcuw'}`;
+//           return config;
+//       },
+//       error => {
+//           return Promise.reject(error);
+//       }
+// );
+// const headers1 = { Authorization: `Bearer ${tokenStr}` };
+// return axios.get(URLConstants.USER_URL, { headers });
+
+let api = axios.create(
     {
-        withCredentials: true,
-        baseURL: `http://localhost:8002/`
+        withCredentials: false,
+        baseURL: `http://localhost:8002/`,
+        headers: {
+            'accept': 'application/json',
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijk5OSIsImV4cCI6MTcxMjI4OTA2MX0.gqX3ePgJj1RCnicL9ARQNLF1kBVJIafoq9oBbla5p3Y',
+        }
     }
 )
 
-export const UsersApi = {
-    // getUsers(currentPage = 1, pageSize = 10) {
-    //     return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-    //     .then (response => response.data)
-    // },
-    // Unfollow(userId = 0) {
-    //     return instance.delete(`follow/${userId}`)
-    //     .then (response => response.data)
-    // },
-    // Follow(userId = 0) {
-    //     return instance.post(`follow/${userId}`)
-    //     .then (response => response.data)
-    // },
-    // authMe() {
-    //     return instance.get('auth/me').then(response => response.data)
-    // },
-    // getProfile(userId) {
-    //     return instance.get('profile/' + userId).then(response => response.data)
-    // }
-    petType(){
-        return instance.get(`api/v1/list_types`)
-    }
+// let t = await fetch(webApiUrl, {
+//   method: 'POST',
+//   headers: {
+//     'Accept': 'application/json',
+//     'Authorization': `Bearer ${tokenStr}`
+//   },
+//   body: ''
+// })
+let webApiUrl = "http://localhost:8002/who_am_i"
+let tokenStr = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijk5OSIsImV4cCI6MTcxMjI4OTA2MX0.gqX3ePgJj1RCnicL9ARQNLF1kBVJIafoq9oBbla5p3Y"
+export default function Test() {
+  const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch(webApiUrl, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Authorization': `Bearer ${tokenStr}`
+            },
+            body: ''
+          })
+        .then((response) => response.json())
+        .then((res) => {
+            setData(res);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+  },[]);
+  console.log(data)
+  return (
+    <div>
+      data
+    </div>
+  );
 }
+
+
+// get requests
+
+
+
+// export const UsersApi = {
+//     getCard(id = 1) {
+//         return api.get('api/v1/get_card/' + id)
+//         .then (response => response.data)
+//         .catch((error) => {
+//             console.log(error);
+//           });
+//     },
+//     getListTypes() {
+//         return api.get('/api/v1/list_types')
+//         .then (response => response.data)
+//         .catch((error) => {
+//             console.log(error);
+//           });
+//     },
+// }
+
+// export default function Test() {
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     api.post(`who_am_i`)
+//       .then((data) => {
+//         setData(data);
+//       })
+//   },[]);
+//   console.log(data)
+//   return (
+//     <div>
+//       data
+//     </div>
+//   );
+// }
