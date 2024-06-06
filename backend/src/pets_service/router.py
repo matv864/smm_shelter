a4b99381d0b9d6a65a0bcfc1d7476ca95d2d40ba
matv864 <matv864@gmail.com>
2024-06-06 10:04:49 +1000

    🚀 nginx is ready

--- backend/src/pets_service/router.py
+++ backend/src/pets_service/router.py
@@ -4,7 +4,7 @@ from src.pets_service.pets import pets_router
 from src.pets_service.pets_type import pets_type_router
 from src.pets_service.images import images_router
 
-main_pets_router = APIRouter()
+main_pets_router = APIRouter(prefix="/api")
 
 main_pets_router.include_router(pets_router)
 main_pets_router.include_router(pets_type_router)
