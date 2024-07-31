7cace95eb9d27f1086e7f907b741a4e56796eb03
1988Artur <larionov.artur@mail.ru>
2024-07-31 23:37:34 +0300

    clean codes out

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,7 +16,4 @@ if (container) {
   console.error("Root element not found");
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
