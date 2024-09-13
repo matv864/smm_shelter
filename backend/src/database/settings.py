b2086aaeae74a22614dc5d193735938102afb79b
matv864 <matv864@gmail.com>
2024-09-14 00:49:45 +1000

    add small docker caching

--- backend/src/database/settings.py
+++ backend/src/database/settings.py
@@ -2,8 +2,6 @@ from dotenv import load_dotenv
 
 from functools import cached_property, lru_cache
 
-from typing import Optional
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 load_dotenv()
@@ -12,9 +10,7 @@ load_dotenv()
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    POSTGRES_NAME_SERVICE: Optional[str] = None
-    POSTGRES_HOST: Optional[str] = None
-    POSTGRES_PORT: Optional[str] = None
+    POSTGRES_HOST: str = None
     POSTGRES_DB: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
@@ -23,28 +19,16 @@ class Settings(BaseSettings):
 
     @cached_property
     def postgres_url(self):
-        if self.POSTGRES_HOST:
-            return f"postgresql+asyncpg://{self.POSTGRES_USER}:" + \
-                f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:" + \
-                f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}" + \
-                "?async_fallback=True"
-
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:" + \
-            f"{self.POSTGRES_PASSWORD}@" + \
-            f"{self.POSTGRES_NAME_SERVICE}/{self.POSTGRES_DB}" + \
+        return "postgresql+asyncpg://" + \
+            f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@" + \
+            f"{self.POSTGRES_HOST}/{self.POSTGRES_DB}" + \
             "?async_fallback=True"
 
     @cached_property
     def command_pg_dump(self):
-        if self.POSTGRES_HOST:
-            return "pg_dump --column-inserts " + \
-                f"-U {self.POSTGRES_USER} " + \
-                f"-h {self.POSTGRES_HOST} -p {self.POSTGRES_PORT} " + \
-                f"{self.POSTGRES_DB} " + \
-                f"> {self.PATH_TO_SAVE_DUMP}"
         return "pg_dump --column-inserts " + \
             f"-U {self.POSTGRES_USER} " + \
-            f"-h {self.POSTGRES_NAME_SERVICE} -p 5432 " + \
+            f"-h {self.POSTGRES_HOST} -p 5432 " + \
             f"{self.POSTGRES_DB} " + \
             f"> {self.PATH_TO_SAVE_DUMP}"
 
